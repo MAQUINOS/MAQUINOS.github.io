8235b01a44db4e627247c2260e552a646f630f5a
Mauricio De Luna <130953908+DeLuna21@users.noreply.github.com>
2024-06-06 20:41:01 -0600

    Update service-worker.js
    si

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,43 @@
-const CACHE_NAME = 'mi-app-cache-v1';
-const urlsToCache = [
+const CACHE_NAME = 'app-shell-cache-v1';
+const URLS_TO_CACHE = [
     '/',
     '/index.html',
     '/css/styles.css',
     '/js/main.js',
     '/manifest.json',
-    '/icon-192x192.png',
-    '/icon-512x512.png'
+    '/images/icon-192x192.png',
+    '/images/icon-512x512.png'
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(function(cache) {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
+            .then(cache => {
+                return cache.addAll(URLS_TO_CACHE);
             })
     );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-            .then(function(response) {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
+            .then(response => {
+                return response || fetch(event.request);
             })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
 });
